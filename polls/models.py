87170b9193707b79775f585a052748a6aa259309
floder <995270418@qq.com>
2017-04-13 17:30:10 +0800

    django demo

---
+++ polls/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+# 投票应用中将创建两个模型: Question和 Choice
+
+# Question两个字段 question_text和publish_date
+class Question(models.Model):
+	question_text = models.CharField(max_length=200)
+	pub_date = models.DateTimeField('date published')
+
+# Choice 两个字段 choice_text 和 votes ,每个Choice和Question都有联系
+class Choice(models.Model):
+	question = models.ForeignKey(Question)
+	choice_text = models.CharField(max_length=200)
+	votes = models.IntegerField(default=0)
+
+	
